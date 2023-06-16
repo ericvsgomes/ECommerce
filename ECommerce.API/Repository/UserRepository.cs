2b22c14090369dc1268af7b99e1c918816c1d13f
Eric Gomes <ericvsg13@gmail.com>
2023-06-16 18:33:22 -0300

    Add UserRepository

---
+++ ECommerce.API/Repository/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using ECommerce.Models;
+
+namespace ECommerce.API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public static List<User> _db = new List<User>();
+
+        public User GetUser(int id)
+        {
+            return _db.Find(x => x.Id == id)!;
+        }
+
+        public List<User> GetUsers()
+        {
+            return _db;
+        }
+
+        public void Add(User user)
+        {
+            _db.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            _db.Remove(GetUser(user.Id));
+            _db.Add(user);
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(GetUser(id));
+        }
+    }
+}
